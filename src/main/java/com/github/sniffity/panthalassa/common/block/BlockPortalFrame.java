a1832cdfbc9728df158bf362ca587c2f52943fdf
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-02 15:05:20 -0500

    Redstone Portal Switch

--- src/main/java/com/github/sniffity/panthalassa/common/block/BlockPortalFrame.java
+++ src/main/java/com/github/sniffity/panthalassa/common/block/BlockPortalFrame.java
@@ -13,18 +13,36 @@ import net.minecraft.world.World;
 
 public class BlockPortalFrame extends Block {
 
-        public BlockPortalFrame() {
-            super(Properties.create(
-                    Material.IRON,
-                    MaterialColor.GRAY)
-                    .hardnessAndResistance(-1.0F,3600000.0F)
-                    .sound(SoundType.METAL));
-        }
+
+    int i = 0;
+
+    public BlockPortalFrame() {
+        super(Properties.create(
+                Material.IRON,
+                MaterialColor.GRAY)
+                .hardnessAndResistance(-1.0F, 3600000.0F)
+                .sound(SoundType.METAL));
+    }
+
+    boolean isPanthalassaPortal(BlockState state) {
+        return state == PanthalassaBlocks.PORTAL.get().getDefaultState();
+    }
 
     public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
+//        if (i <= 0) {
         if (worldIn.isBlockPowered(pos)) {
-            ((BlockPortal) PanthalassaBlocks.PORTAL.get()).trySpawnPortal(worldIn, pos);
-
+            if (isPanthalassaPortal(worldIn.getBlockState(pos.add(1, 0, 0))) ||
+                    isPanthalassaPortal(worldIn.getBlockState(pos.add(-1, 0, 0))) ||
+                    isPanthalassaPortal(worldIn.getBlockState(pos.add(0, 0, -1))) ||
+                    isPanthalassaPortal(worldIn.getBlockState(pos.add(0, 0, 1)))) {
+                if (((BlockPortal) PanthalassaBlocks.PORTAL.get()).tryDestoyPortal(worldIn, pos)) {
+                    //  i = 200;
+                }
+            } else if (((BlockPortal) PanthalassaBlocks.PORTAL.get()).trySpawnPortal(worldIn, pos)) {
+//            i = 200;
+            }
         }
     }
 }
+//        i--;
+//    }
