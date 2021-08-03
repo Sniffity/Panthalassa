cfd1a68903dd1f6f4a491c44bfc785c18a323023
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-03 07:31:40 -0500

    Initial vehicle implementation work

---
+++ src/main/java/com/github/sniffity/panthalassa/vehicle/PanthalassaVehicle.java
@@ -0,0 +1,189 @@
+/*
+package com.github.sniffity.panthalassa.vehicle;
+
+import net.minecraft.entity.*;
+import net.minecraft.entity.passive.WaterMobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.play.client.CSteerBoatPacket;
+import net.minecraft.util.*;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.Constants;
+import net.minecraft.entity.Entity;
+import PanthalassaVehicle2;
+
+import java.util.List;
+
+public abstract class PanthalassaVehicle extends Entity {
+
+    private PanthalassaVehicle2.Status status;
+
+
+    public PanthalassaVehicle(EntityType<?> entityTypeIn, World worldIn) {
+        super(entityTypeIn, worldIn);
+    }
+
+    @Override
+    public ActionResultType processInitialInteract(PlayerEntity player, Hand hand) {
+        if (!world.isRemote && !player.isCrouching()) {
+            if (this.canRide(player)) {
+                player.startRiding(this);
+                return ActionResultType.SUCCESS;
+            }
+        }
+        return ActionResultType.SUCCESS;
+    }
+
+
+    @Override
+    protected void readAdditional(CompoundNBT compound) {
+        if (compound.contains("MaxHealth", Constants.NBT.TAG_FLOAT)) {
+            this.setMaxHealth(compound.getFloat("MaxHealth"));
+        }
+        if (compound.contains("Health", Constants.NBT.TAG_FLOAT)) {
+            this.setHealth(compound.getFloat("Health"));
+        }
+        if (compound.contains("Armor", Constants.NBT.TAG_FLOAT)) {
+            this.setArmor(compound.getFloat("Armor"));
+        }
+    }
+
+    @Override
+    protected void writeAdditional(CompoundNBT compound) {
+        compound.putFloat("MaxHealth", this.getMaxHealth());
+        compound.putFloat("Health", this.getHealth());
+        compound.putFloat("Armor", this.getArmor());
+
+    }
+
+
+
+    protected void vehicleDestroy(LivingEntity entity)
+    {
+        //Play sound
+        //Chat message?
+
+    }
+
+    private void tickLerp() {
+        if (this.canPassengerSteer()) {
+            this.lerpSteps = 0;
+            this.setPacketCoordinates(this.getPosX(), this.getPosY(), this.getPosZ());
+        }
+
+        if (this.lerpSteps > 0) {
+            double d0 = this.getPosX() + (this.lerpX - this.getPosX()) / (double)this.lerpSteps;
+            double d1 = this.getPosY() + (this.lerpY - this.getPosY()) / (double)this.lerpSteps;
+            double d2 = this.getPosZ() + (this.lerpZ - this.getPosZ()) / (double)this.lerpSteps;
+            double d3 = MathHelper.wrapDegrees(this.lerpYaw - (double)this.rotationYaw);
+            this.rotationYaw = (float)((double)this.rotationYaw + d3 / (double)this.lerpSteps);
+            this.rotationPitch = (float)((double)this.rotationPitch + (this.lerpPitch - (double)this.rotationPitch) / (double)this.lerpSteps);
+            --this.lerpSteps;
+            this.setPosition(d0, d1, d2);
+            this.setRotation(this.rotationYaw, this.rotationPitch);
+        }
+    }
+
+    public void tick() {
+        this.previousStatus = this.status;
+        this.status = this.getVehicleStatus();
+        if (this.status != BoatEntity.Status.UNDER_WATER && this.status != BoatEntity.Status.UNDER_FLOWING_WATER) {
+            this.outOfControlTicks = 0.0F;
+        } else {
+            ++this.outOfControlTicks;
+        }
+
+        if (!this.world.isRemote && this.outOfControlTicks >= 60.0F) {
+            this.removePassengers();
+        }
+
+        if (this.getTimeSinceHit() > 0) {
+            this.setTimeSinceHit(this.getTimeSinceHit() - 1);
+        }
+
+        if (this.getDamageTaken() > 0.0F) {
+            this.setDamageTaken(this.getDamageTaken() - 1.0F);
+        }
+
+        super.tick();
+        this.tickLerp();
+        if (this.canPassengerSteer()) {
+            if (this.getPassengers().isEmpty() || !(this.getPassengers().get(0) instanceof PlayerEntity)) {
+                this.setPaddleState(false, false);
+            }
+
+            this.updateMotion();
+            if (this.world.isRemote) {
+                this.controlBoat();
+                this.world.sendPacketToServer(new CSteerBoatPacket(this.getPaddleState(0), this.getPaddleState(1)));
+            }
+
+            this.move(MoverType.SELF, this.getMotion());
+        } else {
+            this.setMotion(Vector3d.ZERO);
+        }
+
+        this.updateRocking();
+
+        for(int i = 0; i <= 1; ++i) {
+            if (this.getPaddleState(i)) {
+                if (!this.isSilent() && (double)(this.paddlePositions[i] % ((float)Math.PI * 2F)) <= (double)((float)Math.PI / 4F) && ((double)this.paddlePositions[i] + (double)((float)Math.PI / 8F)) % (double)((float)Math.PI * 2F) >= (double)((float)Math.PI / 4F)) {
+                    SoundEvent soundevent = this.getPaddleSound();
+                    if (soundevent != null) {
+                        Vector3d vector3d = this.getLook(1.0F);
+                        double d0 = i == 1 ? -vector3d.z : vector3d.z;
+                        double d1 = i == 1 ? vector3d.x : -vector3d.x;
+                        this.world.playSound((PlayerEntity)null, this.getPosX() + d0, this.getPosY(), this.getPosZ() + d1, soundevent, this.getSoundCategory(), 1.0F, 0.8F + 0.4F * this.rand.nextFloat());
+                    }
+                }
+
+                this.paddlePositions[i] = (float)((double)this.paddlePositions[i] + (double)((float)Math.PI / 8F));
+            } else {
+                this.paddlePositions[i] = 0.0F;
+            }
+        }
+
+        this.doBlockCollisions();
+        List<Entity> list = this.world.getEntitiesInAABBexcluding(this, this.getBoundingBox().grow((double)0.2F, (double)-0.01F, (double)0.2F), EntityPredicates.pushableBy(this));
+        if (!list.isEmpty()) {
+            boolean flag = !this.world.isRemote && !(this.getControllingPassenger() instanceof PlayerEntity);
+
+            for(int j = 0; j < list.size(); ++j) {
+                Entity entity = list.get(j);
+                if (!entity.isPassenger(this)) {
+                    if (flag && this.getPassengers().size() < 2 && !entity.isPassenger() && entity.getWidth() < this.getWidth() && entity instanceof LivingEntity && !(entity instanceof WaterMobEntity) && !(entity instanceof PlayerEntity)) {
+                        entity.startRiding(this);
+                    } else {
+                        this.applyEntityCollision(entity);
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    private BoatEntity.Status getBoatStatus() {
+        BoatEntity.Status boatentity$status = this.getUnderwaterStatus();
+        if (boatentity$status != null) {
+            this.waterLevel = this.getBoundingBox().maxY;
+            return boatentity$status;
+        } else if (this.checkInWater()) {
+            return BoatEntity.Status.IN_WATER;
+        } else {
+            float f = this.getVehicleGlide();
+            if (f > 0.0F) {
+                this.boatGlide = f;
+                return BoatEntity.Status.ON_LAND;
+            } else {
+                return BoatEntity.Status.IN_AIR;
+            }
+        }
+    }
+
+
+}
+
+ */
