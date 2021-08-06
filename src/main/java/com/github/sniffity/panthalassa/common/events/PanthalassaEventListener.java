d36da1f575a0b50586757d5aaf74fb3bf4993dfd
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-06 01:46:26 -0500

    Subscribe Events - Vehicles - PlayerDamage + BlockBreak

---
+++ src/main/java/com/github/sniffity/panthalassa/common/events/PanthalassaEventListener.java
@@ -0,0 +1,40 @@
+package com.github.sniffity.panthalassa.common.events;
+
+import com.github.sniffity.panthalassa.Panthalassa;
+import com.github.sniffity.panthalassa.vehicle.PanthalassaVehicle;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Panthalassa.MODID)
+
+public class PanthalassaEventListener {
+
+    @SubscribeEvent
+    public static void onBlockBreak(BlockEvent.BreakEvent event){
+        PlayerEntity player = event.getPlayer();
+        if(player != null){
+            Entity vehicle = player.getRidingEntity();
+            if(vehicle instanceof PanthalassaVehicle){
+                event.setCanceled(true);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerDamage(LivingHurtEvent event){
+        if (event.getEntityLiving() instanceof PlayerEntity) {
+           PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+        if(player != null) {
+            Entity vehicle = player.getRidingEntity();
+            if (vehicle instanceof PanthalassaVehicle) {
+                event.setCanceled(true);
+                vehicle.attackEntityFrom(event.getSource(),event.getAmount());
+            }
+        }
+        }
+    }
+}
