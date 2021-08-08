7534f75cdbc3f39345bb3ad0e9b82e9c3b416c3d
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-07 19:18:31 -0500

    KeyInput Event - update

--- src/main/java/com/github/sniffity/panthalassa/client/events/KeyInputEvent.java
+++ src/main/java/com/github/sniffity/panthalassa/client/events/KeyInputEvent.java
@@ -1,21 +1,33 @@
 package com.github.sniffity.panthalassa.client.events;
 
 import com.github.sniffity.panthalassa.client.ClientHandler;
+import com.github.sniffity.panthalassa.server.network.PanthalassaPacketHandler;
+import com.github.sniffity.panthalassa.server.network.packets.PacketVehicleLights;
+import com.github.sniffity.panthalassa.server.network.packets.PacketVehicleSpecial;
+import com.github.sniffity.panthalassa.server.vehicle.PanthalassaVehicle;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class KeyInputEvent {
 
     @SubscribeEvent
-    public void onKeyInput(InputEvent.KeyInputEvent event)
-    {
-        if (ClientHandler.KEY_VEHICLE_SPECIAL.isKeyDown())
-        {
-            System.out.println("Vehicle Lights Keybind recognized!");
+    public void onKeyInput(InputEvent.KeyInputEvent event) {
+        Minecraft game = Minecraft.getInstance();
+        if (game.player != null) {
+            Entity vehicle = getVehicle(game.player);
+            if (vehicle instanceof PanthalassaVehicle) {
+                if (ClientHandler.KEY_VEHICLE_SPECIAL.isKeyDown()) {
+                    PanthalassaPacketHandler.INSTANCE.sendToServer(new PacketVehicleSpecial());
+                }
+                if (ClientHandler.KEY_VEHICLE_LIGHTS.isPressed()) {
+                    PanthalassaPacketHandler.INSTANCE.sendToServer(new PacketVehicleLights());
+                }
+            }
         }
-        if (ClientHandler.KEY_VEHICLE_LIGHTS.isPressed())
-        {
-            System.out.println("Vehicle Special Keybind recognized!");
-        }
+    }
+    public static Entity getVehicle(Entity player) {
+        return player.getRidingEntity();
     }
 }
