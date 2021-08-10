14e3571ec8879df4cf737f9b8af4bacd1cdb7bc0
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-10 17:01:38 -0500

    Structures registry

---
+++ src/main/java/com/github/sniffity/panthalassa/server/registry/PanthalassaStructures.java
@@ -0,0 +1,75 @@
+package com.github.sniffity.panthalassa.server.registry;
+
+import com.github.sniffity.panthalassa.Panthalassa;
+import com.github.sniffity.panthalassa.server.world.gen.structure.StructurePanthalassaFossil;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.village.PointOfInterestType;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.settings.DimensionStructuresSettings;
+import net.minecraft.world.gen.settings.StructureSeparationSettings;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PanthalassaStructures {
+
+    public static final DeferredRegister<Structure<?>> STRUCTURES = DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, Panthalassa.MODID);
+
+
+    public static final RegistryObject<Structure<NoFeatureConfig>> PANTHALASSA_FOSSIL = STRUCTURES.register("panthalassa_fossil", () -> (new StructurePanthalassaFossil(NoFeatureConfig.CODEC)));
+
+    public static void setupStructures() {
+        setupMapSpacingAndLand(
+                PANTHALASSA_FOSSIL.get(),
+                new StructureSeparationSettings(2,
+                        1,
+                        42424242),
+                false);
+
+    }
+
+    public static <F extends Structure<?>> void setupMapSpacingAndLand(
+            F structure,
+            StructureSeparationSettings structureSeparationSettings,
+            boolean transformSurroundingLand)
+    {
+
+        Structure.STRUCTURES_REGISTRY.put(structure.getRegistryName().toString(), structure);
+
+        if(transformSurroundingLand){
+            Structure.NOISE_AFFECTING_FEATURES =
+                    ImmutableList.<Structure<?>>builder()
+                            .addAll(Structure.NOISE_AFFECTING_FEATURES)
+                            .add(structure)
+                            .build();
+        }
+
+
+        DimensionStructuresSettings.DEFAULTS =
+                ImmutableMap.<Structure<?>, StructureSeparationSettings>builder()
+                        .putAll(DimensionStructuresSettings.DEFAULTS)
+                        .put(structure, structureSeparationSettings)
+                        .build();
+
+
+
+        WorldGenRegistries.NOISE_GENERATOR_SETTINGS.entrySet().forEach(settings -> {
+            Map<Structure<?>, StructureSeparationSettings> structureMap = settings.getValue().structureSettings().structureConfig();
+
+
+            if(structureMap instanceof ImmutableMap){
+                Map<Structure<?>, StructureSeparationSettings> tempMap = new HashMap<>(structureMap);
+                tempMap.put(structure, structureSeparationSettings);
+                settings.getValue().structureSettings().structureConfig = tempMap;
+            }
+            else{
+                structureMap.put(structure, structureSeparationSettings);
+            }
+        });
+    }
