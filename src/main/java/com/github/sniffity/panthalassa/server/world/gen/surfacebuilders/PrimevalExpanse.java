df00601e2777fd3d895ac1c9991692a01d41264f
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-11 03:37:25 -0500

    (PE) - refactoring

--- src/main/java/com/github/sniffity/panthalassa/server/world/gen/surfacebuilders/PrimevalExpanse.java
+++ src/main/java/com/github/sniffity/panthalassa/server/world/gen/surfacebuilders/PrimevalExpanse.java
@@ -8,17 +8,17 @@ import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
 import net.minecraft.world.gen.surfacebuilders.ValleySurfaceBuilder;
 
 public class PrimevalExpanse extends ValleySurfaceBuilder {
-    private static final BlockState ROOF_BLOCK_1 = PanthalassaBlocks.PANTHALASSA_STONE.get().defaultBlockState();
-    private static final BlockState ROOF_BLOCK_2 = PanthalassaBlocks.PANTHALASSA_COARSE_STONE.get().defaultBlockState();
-    private static final BlockState ROOF_BLOCK_3 = PanthalassaBlocks.PANTHALASSA_LOOSE_STONE.get().defaultBlockState();
+    private static final BlockState CEILING_BLOCK_1 = PanthalassaBlocks.PANTHALASSA_STONE.get().defaultBlockState();
+    private static final BlockState CEILING_BLOCK_2 = PanthalassaBlocks.PANTHALASSA_COARSE_STONE.get().defaultBlockState();
+    private static final BlockState CEILING_BLOCK_3 = PanthalassaBlocks.PANTHALASSA_LOOSE_STONE.get().defaultBlockState();
     private static final BlockState FLOOR_BLOCK_1 = PanthalassaBlocks.PANTHALASSA_SOIL.get().defaultBlockState();
     private static final BlockState FLOOR_BLOCK_2 = PanthalassaBlocks.PANTHALASSA_COARSE_SOIL.get().defaultBlockState();
     private static final BlockState FLOOR_BLOCK_3 = PanthalassaBlocks.PANTHALASSA_LOOSE_SOIL.get().defaultBlockState();
 
-    private static final BlockState GRAVEL = PanthalassaBlocks.PANTHALASSA_SOIL.get().defaultBlockState();
+    private static final BlockState PATCH = PanthalassaBlocks.PANTHALASSA_SOIL.get().defaultBlockState();
 
     private static final ImmutableList<BlockState> FLOOR_BLOCK_STATES = ImmutableList.of(FLOOR_BLOCK_1, FLOOR_BLOCK_2, FLOOR_BLOCK_3);
-    private static final ImmutableList<BlockState> CEILING_BLOCK_STATES = ImmutableList.of(ROOF_BLOCK_1, ROOF_BLOCK_2, ROOF_BLOCK_3);
+    private static final ImmutableList<BlockState> CEILING_BLOCK_STATES = ImmutableList.of(CEILING_BLOCK_1, CEILING_BLOCK_2, CEILING_BLOCK_3);
 
     public PrimevalExpanse(Codec<SurfaceBuilderConfig> p_i232123_1_) {
         super(p_i232123_1_);
@@ -33,6 +33,6 @@ public class PrimevalExpanse extends ValleySurfaceBuilder {
     }
 
     protected BlockState getPatchBlockState() {
-        return GRAVEL;
+        return PATCH;
     }
 }
