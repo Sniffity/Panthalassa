012d949cb1477df76509800d58c83873e8a0f574
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-11 03:38:26 -0500

    Feature Registry update

--- src/main/java/com/github/sniffity/panthalassa/server/registry/PanthalassaFeatures.java
+++ src/main/java/com/github/sniffity/panthalassa/server/registry/PanthalassaFeatures.java
@@ -1,9 +1,7 @@
 package com.github.sniffity.panthalassa.server.registry;
 
 import com.github.sniffity.panthalassa.Panthalassa;
-import com.github.sniffity.panthalassa.server.world.gen.feature.FeaturePanthalassaKelp;
-import com.github.sniffity.panthalassa.server.world.gen.feature.FeaturePanthalassaRocks;
-import com.github.sniffity.panthalassa.server.world.gen.feature.FeaturePanthalassaSeaGrass;
+import com.github.sniffity.panthalassa.server.world.gen.feature.*;
 import net.minecraft.world.gen.feature.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,11 +11,17 @@ public class PanthalassaFeatures {
 
     public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES, Panthalassa.MODID);
 
-    public static final RegistryObject<Feature<NoFeatureConfig>> PANTHALASSA_KELP =
-            FEATURES.register("panthalassa_kelp", () -> new FeaturePanthalassaKelp(NoFeatureConfig.CODEC));
+    public static final RegistryObject<Feature<NoFeatureConfig>> PRIMEVAL_EXPANSE_KELP =
+            FEATURES.register("primeval_expanse_kelp", () -> new FeaturePrimevalExpanseKelp(NoFeatureConfig.CODEC));
+
+    public static final RegistryObject<Feature<NoFeatureConfig>> ABYSSAL_OVERGROWTH_KELP =
+            FEATURES.register("abyssal_overgrowth_kelp", () -> new FeatureAbyssalOvergrowthKelp(NoFeatureConfig.CODEC));
 
     public static final RegistryObject<Feature<ProbabilityConfig>> PANTHALASSA_SEA_GRASS =
             FEATURES.register("panthalassa_sea_grass", () -> new FeaturePanthalassaSeaGrass(ProbabilityConfig.CODEC));
+
+    public static final RegistryObject<Feature<NoFeatureConfig>> PRIMORDIAL_STALKS =
+            FEATURES.register("primordial_stalks", () -> new FeaturePrimordialStalks(NoFeatureConfig.CODEC));
 
     public static final RegistryObject<Feature<BlockStateFeatureConfig>> PANTHALASSA_ROCKS =
             FEATURES.register("panthalassa_rocks", () -> new FeaturePanthalassaRocks(BlockStateFeatureConfig.CODEC));
