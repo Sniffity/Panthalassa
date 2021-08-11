5c6d13627ab5e41f80c9d41b5df7767c68f54c30
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-11 03:41:17 -0500

    (PP - Implementation

---
+++ src/main/java/com/github/sniffity/panthalassa/server/world/gen/surfacebuilders/PlaceholderPlaceholder.java
@@ -0,0 +1,40 @@
+package com.github.sniffity.panthalassa.server.world.gen.surfacebuilders;
+
+import com.github.sniffity.panthalassa.server.registry.PanthalassaBlocks;
+import com.google.common.collect.ImmutableList;
+import com.mojang.serialization.Codec;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
+import net.minecraft.world.gen.surfacebuilders.ValleySurfaceBuilder;
+
+public class PlaceholderPlaceholder extends ValleySurfaceBuilder {
+    private static final BlockState CEILING_BLOCK_1 = Blocks.LIGHT_GRAY_GLAZED_TERRACOTTA.defaultBlockState();
+    private static final BlockState CEILING_BLOCK_2 = Blocks.RED_GLAZED_TERRACOTTA.defaultBlockState();
+    private static final BlockState CEILING_BLOCK_3 = Blocks.MAGENTA_GLAZED_TERRACOTTA.defaultBlockState();
+    private static final BlockState FLOOR_BLOCK_1 = Blocks.ORANGE_GLAZED_TERRACOTTA.defaultBlockState();
+    private static final BlockState FLOOR_BLOCK_2 = Blocks.PINK_GLAZED_TERRACOTTA.defaultBlockState();
+    private static final BlockState FLOOR_BLOCK_3 = Blocks.WHITE_GLAZED_TERRACOTTA.defaultBlockState();
+
+    private static final BlockState PATCH = PanthalassaBlocks.PANTHALASSA_SOIL.get().defaultBlockState();
+
+    private static final ImmutableList<BlockState> FLOOR_BLOCK_STATES = ImmutableList.of(FLOOR_BLOCK_1, FLOOR_BLOCK_2, FLOOR_BLOCK_3);
+    private static final ImmutableList<BlockState> CEILING_BLOCK_STATES = ImmutableList.of(CEILING_BLOCK_1, CEILING_BLOCK_2, CEILING_BLOCK_3);
+
+    public PlaceholderPlaceholder(Codec<SurfaceBuilderConfig> p_i232123_1_) {
+        super(p_i232123_1_);
+    }
+
+    protected ImmutableList<BlockState> getFloorBlockStates() {
+        return FLOOR_BLOCK_STATES;
+    }
+
+    protected ImmutableList<BlockState> getCeilingBlockStates() {
+        return CEILING_BLOCK_STATES;
+    }
+
+    protected BlockState getPatchBlockState() {
+        return PATCH;
+    }
+}
+
