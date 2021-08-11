aba749ba8ce3b1b0c31a685b1a49ff112af3fb72
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-11 03:41:44 -0500

    (AO + PE) Kelp separation

---
+++ src/main/java/com/github/sniffity/panthalassa/server/world/gen/feature/FeatureAbyssalOvergrowthKelp.java
@@ -0,0 +1,51 @@
+package com.github.sniffity.panthalassa.server.world.gen.feature;
+
+import com.mojang.serialization.Codec;
+import java.util.Random;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.KelpTopBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+
+public class FeatureAbyssalOvergrowthKelp extends Feature<NoFeatureConfig> {
+    public FeatureAbyssalOvergrowthKelp(Codec<NoFeatureConfig> p_i231967_1_) {
+        super(p_i231967_1_);
+    }
+
+    @Override
+    public boolean place(ISeedReader reader, ChunkGenerator generator, Random rand, BlockPos pos, NoFeatureConfig config) {
+        int i = 0;
+        int kelp_limit = 128;
+        double r = Math.floor(Math.random()*(40)+20);
+        BlockPos blockpos = new BlockPos(pos.getX(), pos.getY()+r, pos.getZ());
+        if ((reader.getBlockState(blockpos).getBlock()== Blocks.WATER)&&(pos.getY()<kelp_limit)) {
+            BlockState blockstate = Blocks.KELP.defaultBlockState();
+            BlockState blockstate1 = Blocks.KELP_PLANT.defaultBlockState();
+            int k = 1 + rand.nextInt(100);
+
+            for(int l = 0; l <= k; ++l) {
+                if (reader.getBlockState(blockpos).is(Blocks.WATER) && reader.getBlockState(blockpos.above()).is(Blocks.WATER) && blockstate1.canSurvive(reader, blockpos) && !reader.getBlockState(blockpos.below()).is(Blocks.KELP)) {
+                    if (l == k) {
+                        reader.setBlock(blockpos, blockstate.setValue(KelpTopBlock.AGE, Integer.valueOf(rand.nextInt(4) + 20)), 2);
+                        ++i;
+                    } else {
+                        reader.setBlock(blockpos, blockstate1, 2);
+                    }
+                } else if (l > 0) {
+                    BlockPos blockpos1 = blockpos.below();
+                    if ((blockstate.canSurvive(reader, blockpos1) && !reader.getBlockState(blockpos1.below()).is(Blocks.KELP))) {
+                        reader.setBlock(blockpos1, blockstate.setValue(KelpTopBlock.AGE, Integer.valueOf(rand.nextInt(4) + 20)), 2);
+                        ++i;
+                    }
+                    break;
+                }
+                blockpos = blockpos.above();
+            }
+        }
+        return i > 0;
+    }
+}
