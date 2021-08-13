c7f86511545842b83693ff1c260da95a33572ba0
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-13 16:03:16 -0500

    Breach AI - Step 2

--- src/main/java/com/github/sniffity/panthalassa/server/entity/creature/ai/PanthalassaBreachAttackGoal.java
+++ src/main/java/com/github/sniffity/panthalassa/server/entity/creature/ai/PanthalassaBreachAttackGoal.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.item.BoatEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.pathfinding.Path;
+import net.minecraft.util.Direction;
 import net.minecraft.util.EntityPredicates;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
@@ -28,6 +29,9 @@ public class PanthalassaBreachAttackGoal extends Goal {
     private boolean step2 = false;
     private LivingEntity target;
     private BlockPos step1Pos;
+    private boolean step1Done;
+    private boolean step2Done;
+    private Vector3d targetPosStep2;
 
 
 
@@ -86,21 +90,31 @@ public class PanthalassaBreachAttackGoal extends Goal {
         this.attacker.setAggressive(true);
         this.delayCounter = 0;
         this.ticksUntilNextAttack = 0;
-        //IF moving to step1 (Flag) do not start again
-        //Check random swimming goal, if already moving do not restart action
-        this.moveStep1();
+        if (!step1Done) {
+            this.moveStep1();
+        }
     }
 
     public void moveStep1(){
+        this.attacker.getLookControl().setLookAt(target.getX(), target.getY()-10, target.getZ());
         this.attacker.getNavigation().moveTo(target.getX(),(target.getY()-10),target.getZ(), this.speedTowardsTarget);
+    }
 
+    public boolean moveStep2(){
+        this.attacker.getLookControl().setLookAt(target.getX(), target.getY(), target.getZ());
+        Vector3d targetPos = new Vector3d (this.target.getX(),this.target.getY(),this.target.getZ());
+        Vector3d attackerPos = new Vector3d (this.attacker.getX(),this.attacker.getY(),this.attacker.getZ());
+        Vector3d attackTrajectory = targetPos.subtract(attackerPos);
+        this.attacker.setDeltaMovement(this.attacker.getDeltaMovement().add(attackTrajectory.normalize().x*0.5, attackTrajectory.normalize().y*0.5, attackTrajectory.normalize().z*0.5));
+        //< attack reach
+        return this.attacker.distanceToSqr(target) < 1;
+        //Hit all enemies in a box around the boat
     }
 
 
     public void stop() {
-
         if (!EntityPredicates.NO_CREATIVE_OR_SPECTATOR.test(target)) {
-            this.attacker.setTarget((LivingEntity)null);
+            this.attacker.setTarget(null);
         }
         this.attacker.setAggressive(false);
         this.attacker.getNavigation().stop();
@@ -109,6 +123,20 @@ public class PanthalassaBreachAttackGoal extends Goal {
 
     @Override
     public void tick() {
+        if (!step1Done) {
+            if ((this.attacker.distanceToSqr(target.getX(), target.getY() - 10, target.getZ())) < 3) {
+                step1Done = true;
+            }
+        } else {
+            if (moveStep2()){
+
+
+
+            }
+        }
+
+
+
 
         //Get distance to step 1 position, if too much do nothing
         //If true, call moveStep2
