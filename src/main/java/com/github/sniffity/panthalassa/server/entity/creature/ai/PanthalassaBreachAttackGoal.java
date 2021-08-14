74225f931b1b0995fdb1234bd72ce17777a2e63f
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-14 01:17:41 -0500

    Breach AI - reach strike position

--- src/main/java/com/github/sniffity/panthalassa/server/entity/creature/ai/PanthalassaBreachAttackGoal.java
+++ src/main/java/com/github/sniffity/panthalassa/server/entity/creature/ai/PanthalassaBreachAttackGoal.java
@@ -13,11 +13,14 @@ import net.minecraft.util.EntityPredicates;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3d;
 import org.lwjgl.system.CallbackI;
 
 import java.util.EnumSet;
 import java.util.List;
+
+import static java.lang.Math.PI;
 
 public class PanthalassaBreachAttackGoal extends Goal {
     protected final CreatureEntity attacker;
@@ -38,6 +41,7 @@ public class PanthalassaBreachAttackGoal extends Goal {
     private boolean step2Done;
     private boolean step3Done;
     private boolean step4Done;
+    private boolean step5Done;
     private Vector3d targetPosStep2;
     private double jumpStart;
 
@@ -54,6 +58,7 @@ public class PanthalassaBreachAttackGoal extends Goal {
     //TODO: Change target to BOAT itself, not PASSENGERS.
     //What happens if passengers abandon boat? Boat should still get destroyed.
 
+    @Override
     public boolean canUse() {
             this.target = this.attacker.getTarget();
             if ((target == null)) {
@@ -71,6 +76,7 @@ public class PanthalassaBreachAttackGoal extends Goal {
 
 
 
+    @Override
     public boolean canContinueToUse() {
         if (boatTarget == null) {
             return false;
@@ -95,6 +101,7 @@ public class PanthalassaBreachAttackGoal extends Goal {
     }
 
 
+    @Override
     public void start() {
         this.attacker.setAggressive(true);
         this.delayCounter = 0;
@@ -108,7 +115,7 @@ public class PanthalassaBreachAttackGoal extends Goal {
 
     public void moveStep1() {
         this.attacker.getLookControl().setLookAt(boatTarget.getX(), boatTarget.getY() - 10, boatTarget.getZ());
-        if ((this.attacker.distanceToSqr(boatTarget.getX(), boatTarget.getY() - 10, boatTarget.getZ())) > 2) {
+        if ((this.attacker.distanceToSqr(boatTarget.getX(), boatTarget.getY() - 10, boatTarget.getZ())) > 4) {
             Vector3d strikePos = new Vector3d(this.boatTarget.getX(), this.boatTarget.getY() - 10, this.boatTarget.getZ());
             this.attacker.getNavigation().moveTo(strikePos.x,strikePos.y,strikePos.z,this.speedTowardsTarget*2);
         } else {
@@ -126,6 +133,7 @@ public class PanthalassaBreachAttackGoal extends Goal {
     }
 
 
+    @Override
     public void stop() {
         if (!EntityPredicates.NO_CREATIVE_OR_SPECTATOR.test(boatTarget)) {
             this.attacker.setTarget(null);
@@ -144,17 +152,22 @@ public class PanthalassaBreachAttackGoal extends Goal {
             }
         } else if (!step3Done) {
             boatTarget.startRiding(attacker);
-            this.attacker.setDeltaMovement(this.attacker.getDeltaMovement().add(0, 1.0D, 0));
+            this.attacker.setDeltaMovement(this.attacker.getDeltaMovement().add(0, 1.80D, 0));
             step3Done = true;
+            this.attacker.xRot = (float)(this.attacker.xRot+(PI/2));
         } else if (!step4Done) {
-            if (attacker.getY()-jumpStart>5) {
+            if (attacker.getY() - jumpStart > 5) {
                 crushBoatandPassengers();
+                this.attacker.xRot = (float)(this.attacker.xRot+(PI/2));
                 step4Done = true;
             }
-
+        } else if (!step5Done && !this.attacker.isInWater()) {
+            this.attacker.xRot = (float) MathHelper.atan2((this.attacker.getDeltaMovement().y),MathHelper.sqrt((this.attacker.getDeltaMovement().x)*(this.attacker.getDeltaMovement().x)+(this.attacker.getDeltaMovement().z)*(this.attacker.getDeltaMovement().z)));
+            if (this.attacker.isInWater()) {
+                step5Done = true;
+            }
         }
     }
-
 
     protected void crushBoatandPassengers() {
         List<Entity> entities = this.attacker.level.getEntities(attacker, new AxisAlignedBB(attacker.getX() - 5, attacker.getY() - 5, attacker.getZ() - 5, attacker.getX() + 5, attacker.getY() + 5, attacker.getZ() + 5));
@@ -166,12 +179,4 @@ public class PanthalassaBreachAttackGoal extends Goal {
         }
     }
 
-
-    protected void resetAttackCooldown() {
-        this.ticksUntilNextAttack = 20;
-    }
-
-    protected double getAttackReachSqr(LivingEntity attackTarget) {
-        return (this.attacker.getBbWidth() * 2.0F * this.attacker.getBbWidth() * 2.0F + attackTarget.getBbWidth());
-    }
 }
