9ed817812cc30f5fd9b54de63ec0c9f2307183fc
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-17 00:48:23 -0500

    Dynamic Pitch - conditions

--- src/main/java/com/github/sniffity/panthalassa/client/model/entity/ModelMegalodon.java
+++ src/main/java/com/github/sniffity/panthalassa/client/model/entity/ModelMegalodon.java
@@ -31,7 +31,7 @@ public class ModelMegalodon extends AnimatedGeoModel<EntityMegalodon> {
     @Override
     public void setLivingAnimations(EntityMegalodon entity, Integer uniqueID, @Nullable AnimationEvent customPredicate) {
         super.setLivingAnimations(entity, uniqueID, customPredicate);
-        if (entity.isInWater()) {
+        if (entity.isInWater() || !entity.isOnGround()) {
             (this.getAnimationProcessor().getBone("main")).setRotationX( (float) (MathHelper.atan2((entity.getDeltaMovement().y),MathHelper.sqrt((entity.getDeltaMovement().x)*(entity.getDeltaMovement().x)+(entity.getDeltaMovement().z)*(entity.getDeltaMovement().z)))));
         }
         (this.getAnimationProcessor().getBone("lower_body_1")).setRotationY((float)(entity.adjustRotation*(PI/180.0F))*5);
