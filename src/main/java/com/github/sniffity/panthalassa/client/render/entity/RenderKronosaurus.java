216f6d2ef07938adf6e72440b44ff0af9970629e
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-20 13:36:34 -0500

    Entity resizing

--- src/main/java/com/github/sniffity/panthalassa/client/render/entity/RenderKronosaurus.java
+++ src/main/java/com/github/sniffity/panthalassa/client/render/entity/RenderKronosaurus.java
@@ -2,8 +2,10 @@ package com.github.sniffity.panthalassa.client.render.entity;
 
 import com.github.sniffity.panthalassa.Panthalassa;
 import com.github.sniffity.panthalassa.client.model.entity.ModelKronosaurus;
+import com.github.sniffity.panthalassa.server.entity.creature.EntityArchelon;
 import com.github.sniffity.panthalassa.server.entity.creature.EntityKronosaurus;
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
@@ -15,5 +17,12 @@ public class RenderKronosaurus extends GeoEntityRenderer<EntityKronosaurus> {
         super(renderManager, new ModelKronosaurus());
         this.shadowRadius = 1.0F;
     }
-
+    @Override
+    public void renderEarly(EntityKronosaurus animatable, MatrixStack stackIn, float ticks,
+                            IRenderTypeBuffer renderTypeBuffer, IVertexBuilder vertexBuilder, int packedLightIn, int packedOverlayIn,
+                            float red, float green, float blue, float partialTicks) {
+        super.renderEarly(animatable, stackIn, ticks, renderTypeBuffer, vertexBuilder, packedLightIn, packedOverlayIn,
+                red, green, blue, partialTicks);
+        stackIn.scale(2.0F, 2.0F, 2.0F);
+    }
 }
