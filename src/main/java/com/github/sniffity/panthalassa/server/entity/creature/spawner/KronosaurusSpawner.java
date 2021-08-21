cef89deda63d467dd20423750803a4aa80de6ea2
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-21 08:59:46 -0500

    Configure Kronosaurus Spawner

--- src/main/java/com/github/sniffity/panthalassa/server/entity/creature/spawner/KronosaurusSpawner.java
+++ src/main/java/com/github/sniffity/panthalassa/server/entity/creature/spawner/KronosaurusSpawner.java
@@ -2,6 +2,7 @@ package com.github.sniffity.panthalassa.server.entity.creature.spawner;
 
 import com.github.sniffity.panthalassa.Panthalassa;
 import com.github.sniffity.panthalassa.server.entity.creature.EntityKronosaurus;
+import com.github.sniffity.panthalassa.server.entity.creature.PanthalassaEntity;
 import com.github.sniffity.panthalassa.server.registry.PanthalassaDimension;
 import com.github.sniffity.panthalassa.server.registry.PanthalassaEntityTypes;
 import net.minecraft.entity.EntitySpawnPlacementRegistry;
@@ -23,80 +24,73 @@ import java.util.Random;
 //Class adapted from Vanilla Minecraft's CatSpawner, still needs some tweaking.
 
 public class KronosaurusSpawner  {
-    private int nextTick;
 
     //The class needs a constructor, so it can be initialized and then called from a static context.
     public KronosaurusSpawner(){
     }
 
-    //This tick method is called on the WorldTick Event.
-    public int tick(ServerWorld worldIn) {
+    //This tick method is called on the WorldTick Event. Returns methods being currently used so the method can stop ticking if conditions are not meant.
+    public void tick(ServerWorld worldIn) {
         if (worldIn.getGameRules().getBoolean(GameRules.RULE_DOMOBSPAWNING)) {
-            --this.nextTick;
-            if (this.nextTick > 0) {
-                return 0;
-            } else {
-                this.nextTick = 1200;
-                PlayerEntity playerentity = worldIn.getRandomPlayer();
-                //Only execute this method in Panthalassa Dimension.
-                if (playerentity == null || (playerentity.level.dimension() != PanthalassaDimension.PANTHALASSA)) {
-                    return 0;
-                } else {
-                    Random random = worldIn.random;
-                    /*From the player, this is casting out two radii: An inner one of 8 blocks, and an outer one of 32 blocks. The entities will spawn somewhere within
-                    the outer and the inner radius
-                    * */
-                    int i = (8 + random.nextInt(24)) * (random.nextBoolean() ? -1 : 1);
-                    int j = (8 + random.nextInt(24)) * (random.nextBoolean() ? -1 : 1);
-                    double r = Math.floor(Math.random() * (81) + 20);
-                    //For Kronosaurus, the idea is to spawn them in packs of 3. So, grab a random BlockPos (blockpos)....
-                    BlockPos blockpos = new BlockPos(playerentity.blockPosition().getX() + i, r, playerentity.blockPosition().getX() + j);
-                    //And then create two other random BlockPos close to that first blockpos.
-                    BlockPos blockpos0 = blockpos.offset((random.nextInt(3) * (random.nextBoolean() ? -1 : 1)), (random.nextInt(3) * (random.nextBoolean() ? -1 : 1)), (random.nextInt(3) * (random.nextBoolean() ? -1 : 1)));
-                    BlockPos blockpos1 = blockpos.offset(1, 1, 1);
+            PlayerEntity playerentity = worldIn.getRandomPlayer();
+            //Only execute this method in Panthalassa Dimension.
+            if (!(playerentity == null || (playerentity.level.dimension() != PanthalassaDimension.PANTHALASSA))) {
+                Random random = worldIn.random;
+                /*From the player, this is casting out two radii on the XZ plane:
+                An inner one of 16 blocks, and an outer one of 80 blocks.
+                The entities will spawn somewhere within the outer and the inner radius, if conditions were met.
+                Y position is entirely randomized.
+                * */
+                double i = (Math.floor(Math.random() * (65) + 16) * ((new Random().nextBoolean()) ? -1 : 1));
+                double j = (Math.floor(Math.random() * (65) + 16) * ((new Random().nextBoolean()) ? -1 : 1));
+                double r = Math.floor(Math.random() * (81) + 20);
+                //For Kronosaurus, the idea is to spawn them in packs of 3. Once spawned, if during random movement two schools ever cross over, school mixing can occurr.
+                // So, grab a random BlockPos (blockpos)....
+                BlockPos blockpos = new BlockPos(playerentity.blockPosition().getX() + i, r, playerentity.blockPosition().getZ() + j);
+                //And then create two other random BlockPos close to that first blockpos.
+                BlockPos blockpos0 = blockpos.offset((random.nextInt(3) * ((new Random().nextBoolean()) ? -1 : 1)), (random.nextInt(3) * ((new Random().nextBoolean()) ? -1 : 1)), (random.nextInt(3) * ((new Random().nextBoolean()) ? -1 : 1)));
+                BlockPos blockpos1 = blockpos.offset((random.nextInt(3) * ((new Random().nextBoolean()) ? -1 : 1)), (random.nextInt(3) * ((new Random().nextBoolean()) ? -1 : 1)), (random.nextInt(3) * ((new Random().nextBoolean()) ? -1 : 1)));
 
-                    //Ensure that all the BlockPos that have been selected are different
-                    if (blockpos != blockpos0 && blockpos != blockpos1 && blockpos0 != blockpos1) {
-                        if (!worldIn.hasChunksAt(blockpos.getX() - 10, blockpos.getY() - 10, blockpos.getZ() - 10, blockpos.getX() + 10, blockpos.getY() + 10, blockpos.getZ() + 10)) {
-                            return 0;
-                        } else {
-                            //If all BlockPos are okay to spawn...
+                //Ensure that all the BlockPos that have been selected are different, and are loaded, and have space around them to spawn the entities without suffocation, and there's an actual water block where to spawn the Kronosaurus....
+                if (blockpos != blockpos0 && blockpos != blockpos1 && blockpos0 != blockpos1) {
+                    if (worldIn.hasChunksAt(blockpos.getX() - 10, blockpos.getY() - 10, blockpos.getZ() - 10, blockpos.getX() + 10, blockpos.getY() + 10, blockpos.getZ() + 10)) {
+                        if ((checkValidSpawnArea(worldIn, blockpos)) || (checkValidSpawnArea(worldIn, blockpos0)) || (checkValidSpawnArea(worldIn, blockpos1))) {
                             if (WorldEntitySpawner.isSpawnPositionOk(EntitySpawnPlacementRegistry.PlacementType.IN_WATER, worldIn, blockpos, PanthalassaEntityTypes.KRONOSAURUS.get())
-                            && WorldEntitySpawner.isSpawnPositionOk(EntitySpawnPlacementRegistry.PlacementType.IN_WATER, worldIn, blockpos0, PanthalassaEntityTypes.KRONOSAURUS.get())
-                            && WorldEntitySpawner.isSpawnPositionOk(EntitySpawnPlacementRegistry.PlacementType.IN_WATER, worldIn, blockpos1, PanthalassaEntityTypes.KRONOSAURUS.get())) {
+                                    && WorldEntitySpawner.isSpawnPositionOk(EntitySpawnPlacementRegistry.PlacementType.IN_WATER, worldIn, blockpos0, PanthalassaEntityTypes.KRONOSAURUS.get())
+                                    && WorldEntitySpawner.isSpawnPositionOk(EntitySpawnPlacementRegistry.PlacementType.IN_WATER, worldIn, blockpos1, PanthalassaEntityTypes.KRONOSAURUS.get())) {
                                 //Proceed to do spawnInBiome...
-                                return this.spawnInBiome(worldIn, blockpos, blockpos0, blockpos1);
-
+                                this.spawnInBiome(worldIn, blockpos, blockpos0, blockpos1);
                             }
-                            return 0;
                         }
-                    } else {
-                        return 0;
                     }
                 }
             }
         }
-        return 0;
     }
 
 
-    private int spawnInBiome(ServerWorld p_221121_1_, BlockPos blockPos, BlockPos blockPos0, BlockPos blockPos1) {
+
+    private void spawnInBiome(ServerWorld worldIn, BlockPos blockPos, BlockPos blockPos0, BlockPos blockPos1) {
         //If for some reason the biome is null, do not spawn...
-        if (p_221121_1_.getBiome(blockPos).getRegistryName() == null) {
-            return 0;
-            //If the biome for the first blockpos is "abyssal_overgrowth"....
-        } else if (Objects.equals(p_221121_1_.getBiome(blockPos).getRegistryName(), new ResourceLocation(Panthalassa.MODID, "abyssal_overgrowth"))){
-            //And there's less than 7 Kronosaurus already spawned (hence, only 2 packs of 3) in the 96*96*96 Bounding Box
-            List<EntityKronosaurus> list = p_221121_1_.getEntitiesOfClass(EntityKronosaurus.class, (new AxisAlignedBB(blockPos)).inflate(96.0D, 96.0D, 96.0D));
-            if (list.size() < 7) {
-                //Proceed to spawn the Kronosaurus pack at the selected BlockPositions
-                return this.spawnKronosaurus(blockPos, blockPos0, blockPos1, p_221121_1_);
+        if (worldIn.getBiome(blockPos).getRegistryName() != null) {
+            //If the biome we're in is the Abyssal Overgrowth....
+            if (Objects.equals(worldIn.getBiome(blockPos).getRegistryName(), new ResourceLocation(Panthalassa.MODID, "abyssal_overgrowth"))) {
+                //And, within a 96x96x96 Bounding Box, there's less than 10 Kronosaurus already spawned... (so basically, a maximum of 3 spawn "groups" of 3 within the 96x96x96 Bounding Box)
+                List<EntityKronosaurus> list = worldIn.getEntitiesOfClass(EntityKronosaurus.class, (new AxisAlignedBB(blockPos)).inflate(96.0D, 96.0D, 96.0D));
+                //...and within a smaller 24x64x24 bounding box there's no other Kronosaurus (ensures separation of same species - separation will be different for same vs. different species)
+                List<EntityKronosaurus> list0 = worldIn.getEntitiesOfClass(EntityKronosaurus.class, (new AxisAlignedBB(blockPos)).inflate(36.0D, 64.0D, 36.0D));
+                //...and within a 12x12x12 bounding box there's no other Panthalassa Entities (ensures separation of different species - separation will be different for same vs. different species)
+                List<PanthalassaEntity> list1 = worldIn.getEntitiesOfClass(PanthalassaEntity.class, (new AxisAlignedBB(blockPos)).inflate(12.0D, 12.0D, 12.0D));
+
+                if (list.size() < 10 && list0.isEmpty() && list1.isEmpty()) {
+                    //Proceed to spawn the Kronosaurus school at the selected BlockPositions
+                    this.spawnKronosaurus(blockPos, blockPos0, blockPos1, worldIn);
+                }
             }
         }
-        return 0;
     }
 
-    private int spawnKronosaurus(BlockPos blockPos, BlockPos blockPos0, BlockPos blockPos1, ServerWorld worldIn) {
+    private void spawnKronosaurus(BlockPos blockPos, BlockPos blockPos0, BlockPos blockPos1, ServerWorld worldIn) {
         EntityKronosaurus kronosaurus = PanthalassaEntityTypes.KRONOSAURUS.get().create(worldIn);
         EntityKronosaurus kronosaurus0 = PanthalassaEntityTypes.KRONOSAURUS.get().create(worldIn);
         EntityKronosaurus kronosaurus1 = PanthalassaEntityTypes.KRONOSAURUS.get().create(worldIn);
@@ -105,27 +99,43 @@ public class KronosaurusSpawner  {
         //Proceed to spawn each Kronosaurus and position them, verifying for possible null values.
         if (kronosaurus != null){
             kronosaurus.moveTo(blockPos, 0.0F, 0.0F);
-            if(net.minecraftforge.common.ForgeHooks.canEntitySpawn(kronosaurus, worldIn, blockPos.getX(), blockPos.getY(), blockPos.getZ(), null, SpawnReason.NATURAL) == -1) return 0;
-            kronosaurus.finalizeSpawn(worldIn, worldIn.getCurrentDifficultyAt(blockPos), SpawnReason.NATURAL, (ILivingEntityData)null, (CompoundNBT)null);
-            worldIn.addFreshEntityWithPassengers(kronosaurus);
-
+            if(net.minecraftforge.common.ForgeHooks.canEntitySpawn(kronosaurus, worldIn, blockPos.getX(), blockPos.getY(), blockPos.getZ(), null, SpawnReason.NATURAL) != -1) {
+                kronosaurus.finalizeSpawn(worldIn, worldIn.getCurrentDifficultyAt(blockPos), SpawnReason.NATURAL, (ILivingEntityData)null, (CompoundNBT)null);
+                worldIn.addFreshEntityWithPassengers(kronosaurus);
+            }
         }
 
         if (kronosaurus0 != null){
             kronosaurus0.moveTo(blockPos0, 0.0F, 0.0F);
-            if(net.minecraftforge.common.ForgeHooks.canEntitySpawn(kronosaurus0, worldIn, blockPos0.getX(), blockPos0.getY(), blockPos0.getZ(), null, SpawnReason.NATURAL) == -1) return 0;
-            kronosaurus0.finalizeSpawn(worldIn, worldIn.getCurrentDifficultyAt(blockPos), SpawnReason.NATURAL, (ILivingEntityData)null, (CompoundNBT)null);
-            worldIn.addFreshEntityWithPassengers(kronosaurus0);
+            if(net.minecraftforge.common.ForgeHooks.canEntitySpawn(kronosaurus0, worldIn, blockPos0.getX(), blockPos0.getY(), blockPos0.getZ(), null, SpawnReason.NATURAL) != -1) {
+                kronosaurus0.finalizeSpawn(worldIn, worldIn.getCurrentDifficultyAt(blockPos), SpawnReason.NATURAL, (ILivingEntityData)null, (CompoundNBT)null);
+                worldIn.addFreshEntityWithPassengers(kronosaurus0);
+            }
 
         }
 
         if (kronosaurus1 != null) {
             kronosaurus1.moveTo(blockPos1, 0.0F, 0.0F);
-            if(net.minecraftforge.common.ForgeHooks.canEntitySpawn(kronosaurus, worldIn, blockPos1.getX(), blockPos1.getY(), blockPos1.getZ(), null, SpawnReason.NATURAL) == -1) return 0;
-            kronosaurus1.finalizeSpawn(worldIn, worldIn.getCurrentDifficultyAt(blockPos1), SpawnReason.NATURAL, (ILivingEntityData)null, (CompoundNBT)null);
-            worldIn.addFreshEntityWithPassengers(kronosaurus1);
+            if(net.minecraftforge.common.ForgeHooks.canEntitySpawn(kronosaurus, worldIn, blockPos1.getX(), blockPos1.getY(), blockPos1.getZ(), null, SpawnReason.NATURAL) != -1) {
+                kronosaurus1.finalizeSpawn(worldIn, worldIn.getCurrentDifficultyAt(blockPos1), SpawnReason.NATURAL, (ILivingEntityData)null, (CompoundNBT)null);
+                worldIn.addFreshEntityWithPassengers(kronosaurus1);
+            }
         }
-        return 1;
+    }
+
+    public boolean checkValidSpawnArea(ServerWorld worldIn, BlockPos blockPos){
+        //Analyze a 3x3x3 cube, around the intended spawn position, to ensure the enttiy will not suffocate.
+        for (int i = -1; i<2; i++) {
+            for (int j = -1; j < 2; j++) {
+                for (int k = -1; k < 2; k++) {
+                    if (worldIn.getBlockState(blockPos.offset(i,j,k)).canOcclude()){
+                        return false;
+                    }
+
+                }
+            }
+        }
+        return true;
     }
 }
 
