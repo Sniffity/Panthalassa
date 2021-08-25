9efcf4feeb363082b7b0c079b903fc8ea4721679
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-25 09:35:25 -0500

    Revert "Revert "Schooling - Leader Test""
    This reverts commit 99fec3bbb5daa5159a5da143943fa36ee8c0fadc.

--- src/main/java/com/github/sniffity/panthalassa/client/render/entity/RenderKronosaurus.java
+++ src/main/java/com/github/sniffity/panthalassa/client/render/entity/RenderKronosaurus.java
@@ -4,11 +4,13 @@ import com.github.sniffity.panthalassa.Panthalassa;
 import com.github.sniffity.panthalassa.client.model.entity.ModelKronosaurus;
 import com.github.sniffity.panthalassa.server.entity.creature.EntityArchelon;
 import com.github.sniffity.panthalassa.server.entity.creature.EntityKronosaurus;
+import com.github.sniffity.panthalassa.server.entity.vehicle.VehicleMRSV;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
 public class RenderKronosaurus extends GeoEntityRenderer<EntityKronosaurus> {
@@ -24,5 +26,10 @@ public class RenderKronosaurus extends GeoEntityRenderer<EntityKronosaurus> {
         super.renderEarly(animatable, stackIn, ticks, renderTypeBuffer, vertexBuilder, packedLightIn, packedOverlayIn,
                 red, green, blue, partialTicks);
         stackIn.scale(2.0F, 2.0F, 2.0F);
+    }
+
+    @Override
+    protected int getBlockLightLevel(EntityKronosaurus entityIn, BlockPos partialTicks) {
+        return entityIn.getIsLeader() ? 15 : 1;
     }
 }
