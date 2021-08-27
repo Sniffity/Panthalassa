6a1bf0735bb06dd366ff6107d2c12f6057ae1d14
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-08-27 09:26:19 -0500

    Fix FeaturePanthalassaRocks + add missing item models

--- src/main/java/com/github/sniffity/panthalassa/server/world/gen/feature/FeaturePanthalassaRocks.java
+++ src/main/java/com/github/sniffity/panthalassa/server/world/gen/feature/FeaturePanthalassaRocks.java
@@ -20,32 +20,33 @@ public class FeaturePanthalassaRocks extends Feature<BlockStateFeatureConfig> {
 
     public boolean place(ISeedReader reader, ChunkGenerator generator, Random rand, BlockPos pos, BlockStateFeatureConfig config) {
         boolean flag = false;
+        double r = Math.floor(Math.random() * (81) + 20);
+        BlockPos pos0 = new BlockPos(pos.getX(), r, pos.getZ());
+
         while(true) {
             if (!flag) {
-                double r = Math.floor(Math.random() * (81) + 20);
-                pos = new BlockPos(pos.getX(), r, pos.getZ());
+                pos0 = new BlockPos(pos0.getX(), pos0.getY(), pos0.getZ());
                 flag = true;
             }
-            double r = Math.floor(Math.random()*(81)+20);
-            pos = new BlockPos(pos.getX(), r, pos.getZ());
+            pos0 = new BlockPos(pos0.getX(), pos0.getY(), pos0.getZ());
             label46: {
-                if (pos.getY() > 3) {
-                    if (reader.getFluidState(pos.below()).is(FluidTags.WATER)) {
+                if (pos0.getY() > 3) {
+                    if (reader.getFluidState(pos0.below()).is(FluidTags.WATER)) {
                         break label46;
                     }
 
                     if (
-                            !(reader.getBlockState(pos.below()) == PanthalassaBlocks.PANTHALASSA_SOIL.get().defaultBlockState())
+                            !(reader.getBlockState(pos0.below()) == PanthalassaBlocks.PANTHALASSA_SOIL.get().defaultBlockState())
                             &&
-                            !(reader.getBlockState(pos.below()) == PanthalassaBlocks.PANTHALASSA_COARSE_SOIL.get().defaultBlockState())
+                            !(reader.getBlockState(pos0.below()) == PanthalassaBlocks.PANTHALASSA_COARSE_SOIL.get().defaultBlockState())
                             &&
-                            !(reader.getBlockState(pos.below()) == PanthalassaBlocks.PANTHALASSA_LOOSE_SOIL.get().defaultBlockState()))
+                            !(reader.getBlockState(pos0.below()) == PanthalassaBlocks.PANTHALASSA_LOOSE_SOIL.get().defaultBlockState()))
                      {
                         break label46;
                     }
                 }
 
-                if (pos.getY() <= 3) {
+                if (pos0.getY() <= 3) {
                     return false;
                 }
 
@@ -55,19 +56,19 @@ public class FeaturePanthalassaRocks extends Feature<BlockStateFeatureConfig> {
                     int k = rand.nextInt(5);
                     float f = (float)(i + j + k) * 0.333F + 0.5F;
 
-                    for(BlockPos blockpos : BlockPos.betweenClosed(pos.offset(-i, -j, -k), pos.offset(i, j, k))) {
-                        if (blockpos.distSqr(pos) <= (double)(f * f)) {
+                    for(BlockPos blockpos : BlockPos.betweenClosed(pos0.offset(-i, -j, -k), pos0.offset(i, j, k))) {
+                        if (blockpos.distSqr(pos0) <= (double)(f * f)) {
                             reader.setBlock(blockpos, config.state, 4);
                         }
                     }
 
-                    pos = pos.offset(-1 + rand.nextInt(2), -rand.nextInt(2), -1 + rand.nextInt(2));
+                    pos0 = pos0.offset(-1 + rand.nextInt(2), -rand.nextInt(2), -1 + rand.nextInt(2));
                 }
 
                 return true;
             }
 
-            pos = pos.below();
+            pos0 = pos0.below();
         }
     }
 }
