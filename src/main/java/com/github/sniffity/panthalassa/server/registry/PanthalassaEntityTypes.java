3b4bca632f95fe0b226f11aa3de1a53558faf9c1
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-12-30 13:38:42 -0500

    Re-class mobs (creature -> underground_water_creature)

--- src/main/java/com/github/sniffity/panthalassa/server/registry/PanthalassaEntityTypes.java
+++ src/main/java/com/github/sniffity/panthalassa/server/registry/PanthalassaEntityTypes.java
@@ -18,38 +18,38 @@ public class PanthalassaEntityTypes {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, Panthalassa.MODID);
 
     public static final RegistryObject<EntityType<EntityKronosaurus>> KRONOSAURUS = ENTITY_TYPES.register ("kronosaurus",()->
-            EntityType.Builder.of(EntityKronosaurus::new,MobCategory.CREATURE)
+            EntityType.Builder.of(EntityKronosaurus::new,MobCategory.UNDERGROUND_WATER_CREATURE)
                     .sized(1.0F, 1.0F)
                     .canSpawnFarFromPlayer()
                     .build(new ResourceLocation(Panthalassa.MODID, "kronosaurus").toString()));
 
     public static final RegistryObject<EntityType<EntityMegalodon>> MEGALODON = ENTITY_TYPES.register ("megalodon",()->
-            EntityType.Builder.of(EntityMegalodon::new,MobCategory.CREATURE)
+            EntityType.Builder.of(EntityMegalodon::new,MobCategory.UNDERGROUND_WATER_CREATURE)
                     .sized(1.0F, 1.0F)
                     .build(new ResourceLocation(Panthalassa.MODID, "megalodon").toString()));
 
     public static final RegistryObject<EntityType<EntityArchelon>> ARCHELON = ENTITY_TYPES.register ("archelon",()->
-            EntityType.Builder.of(EntityArchelon::new,MobCategory.CREATURE)
+            EntityType.Builder.of(EntityArchelon::new,MobCategory.UNDERGROUND_WATER_CREATURE)
                     .sized(1.0F, 1.0F)
                     .build(new ResourceLocation(Panthalassa.MODID, "archelon").toString()));
 
     public static final RegistryObject<EntityType<EntityMosasaurus>> MOSASAURUS = ENTITY_TYPES.register ("mosasaurus",()->
-            EntityType.Builder.of(EntityMosasaurus::new,MobCategory.CREATURE)
+            EntityType.Builder.of(EntityMosasaurus::new,MobCategory.UNDERGROUND_WATER_CREATURE)
                     .sized(1.0F, 1.0F)
                     .build(new ResourceLocation(Panthalassa.MODID, "mosasaurus").toString()));
 
     public static final RegistryObject<EntityType<EntityCoelacanth>> COELACANTH = ENTITY_TYPES.register ("coelacanth",()->
-            EntityType.Builder.of(EntityCoelacanth::new,MobCategory.CREATURE)
+            EntityType.Builder.of(EntityCoelacanth::new,MobCategory.UNDERGROUND_WATER_CREATURE)
                     .sized(1.0F, 1.0F)
                     .build(new ResourceLocation(Panthalassa.MODID, "coelacanth").toString()));
 
     public static final RegistryObject<EntityType<EntityDunkleosteus>> DUNKLEOSTEUS = ENTITY_TYPES.register ("dunkleosteus",()->
-            EntityType.Builder.of(EntityDunkleosteus::new,MobCategory.CREATURE)
+            EntityType.Builder.of(EntityDunkleosteus::new,MobCategory.UNDERGROUND_WATER_CREATURE)
                     .sized(1.0F, 1.0F)
                     .build(new ResourceLocation(Panthalassa.MODID, "dunkleosteus").toString()));
 
     public static final RegistryObject<EntityType<EntityLeedsichthys>> LEEDSICHTHYS = ENTITY_TYPES.register ("leedsichthys",()->
-            EntityType.Builder.of(EntityLeedsichthys::new,MobCategory.CREATURE)
+            EntityType.Builder.of(EntityLeedsichthys::new,MobCategory.UNDERGROUND_WATER_CREATURE)
                     .sized(1.0F, 1.0F)
                     .build(new ResourceLocation(Panthalassa.MODID, "leedsichthys").toString()));
 
@@ -67,10 +67,6 @@ public class PanthalassaEntityTypes {
                     .build(new ResourceLocation(Panthalassa.MODID, "abyss_glider_2_submersible_vehicle").toString()));
 
     public static void spawnPlacements() {
-        Heightmap.Types.MOTION_BLOCKING_NO_LEAVES.isOpaque = (state) -> {
-            return (state.getMaterial().blocksMotion() || !state.getFluidState().isEmpty()) && !(state.getBlock() == PanthalassaBlocks.PANTHALASSA_WATER.get());
-        };
-
         SpawnPlacements.register(KRONOSAURUS.get(), SpawnPlacements.Type.IN_WATER, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, PanthalassaEntity::canPanthalassaEntitySpawn);
         SpawnPlacements.register(MOSASAURUS.get(), SpawnPlacements.Type.IN_WATER, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, PanthalassaEntity::canPanthalassaEntitySpawn);
         SpawnPlacements.register(COELACANTH.get(), SpawnPlacements.Type.IN_WATER, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, PanthalassaEntity::canPanthalassaEntitySpawn);
