2c5c5c028ac528046440fa1c1adc7aeb4c99bab7
Sniffity <66455728+Sniffity@users.noreply.github.com>
2021-12-31 13:43:14 -0500

    Implement new M asset

--- src/main/java/com/github/sniffity/panthalassa/client/model/entity/ModelMegalodon.java
+++ src/main/java/com/github/sniffity/panthalassa/client/model/entity/ModelMegalodon.java
@@ -18,7 +18,13 @@ public class ModelMegalodon extends AnimatedGeoModel<EntityMegalodon> {
 
     @Override
     public ResourceLocation getTextureLocation(EntityMegalodon object) {
-        return new ResourceLocation(Panthalassa.MODID,"textures/creature/megalodon/megalodon.png");
+        if (object.getTextureVariant() == 0){
+            return new ResourceLocation(Panthalassa.MODID,"textures/creature/megalodon/megalodon_white.png");
+        } else if (object.getTextureVariant() == 1) {
+            return new ResourceLocation(Panthalassa.MODID,"textures/creature/megalodon/megalodon_pink.png");
+        } else {
+            return new ResourceLocation(Panthalassa.MODID,"textures/creature/megalodon/megalodon_green.png");
+        }
     }
 
     @Override
@@ -30,11 +36,11 @@ public class ModelMegalodon extends AnimatedGeoModel<EntityMegalodon> {
     public void setLivingAnimations(EntityMegalodon entity, Integer uniqueID, @Nullable AnimationEvent customPredicate) {
         super.setLivingAnimations(entity, uniqueID, customPredicate);
         if ((entity.isInWater() && !entity.level.getBlockState(entity.blockPosition().below()).canOcclude()) || entity.getBreaching()) {
-            (this.getAnimationProcessor().getBone("main")).setRotationX(((float) Mth.atan2((entity.getDeltaMovement().y), Mth.sqrt((float) ((entity.getDeltaMovement().x) * (entity.getDeltaMovement().x) + (entity.getDeltaMovement().z) * (entity.getDeltaMovement().z))))));
+            (this.getAnimationProcessor().getBone("neck")).setRotationX(((float) Mth.atan2((entity.getDeltaMovement().y), Mth.sqrt((float) ((entity.getDeltaMovement().x) * (entity.getDeltaMovement().x) + (entity.getDeltaMovement().z) * (entity.getDeltaMovement().z))))));
         }
-        (this.getAnimationProcessor().getBone("lower_body_1")).setRotationY((float) (entity.adjustYaw * (PI / 180.0F)) * 5.0F);
-        (this.getAnimationProcessor().getBone("lower_body_3")).setRotationY((float) (entity.adjustYaw * (PI / 180.0F)) * 5.0F);
-        (this.getAnimationProcessor().getBone("lower_body_6")).setRotationY((float) (entity.adjustYaw * (PI / 180.0F)) * 5.0F);
+        (this.getAnimationProcessor().getBone("tail1")).setRotationY((float) (entity.adjustYaw * (PI / 180.0F)) * 5.0F);
+        (this.getAnimationProcessor().getBone("tail2")).setRotationY((float) (entity.adjustYaw * (PI / 180.0F)) * 5.0F);
+        (this.getAnimationProcessor().getBone("tail3")).setRotationY((float) (entity.adjustYaw * (PI / 180.0F)) * 5.0F);
         (this.getAnimationProcessor().getBone("head")).setRotationY((float) (-entity.adjustYaw * (PI / 180.0F)) * 5.0F);
     }
 
