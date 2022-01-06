56e89259c442ed70ae7bc17f03530b3496e8a56f
Sniffity <66455728+Sniffity@users.noreply.github.com>
2022-01-06 15:34:06 -0500

    Refactoring + typo corrections

--- src/main/java/com/github/sniffity/panthalassa/config/PanthalassaCommonConfig.java
+++ src/main/java/com/github/sniffity/panthalassa/config/PanthalassaCommonConfig.java
@@ -2,7 +2,7 @@ package com.github.sniffity.panthalassa.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
-public class PanthalassaServerConfig {
+public class PanthalassaCommonConfig {
 
     public static final ForgeConfigSpec GENERAL_SPEC;
 
@@ -13,17 +13,13 @@ public class PanthalassaServerConfig {
     }
 
     private static void setupConfig(ForgeConfigSpec.Builder builder) {
-        builder.comment("Panthalassa Server Config");
-        builder.push("Give Journan");
+        builder.comment("Panthalassa Common Config");
+        builder.push("Give Journal");
         builder.comment("This boolean value will determine whether or not the Panthalassa Journal (Guide Book) is given to players when they first join the server");
         giveJournal = builder
                 .define("give_journal", true);
         builder.pop();
-
     }
 
-
     public static ForgeConfigSpec.BooleanValue giveJournal;
-
-
 }
