ea78f3fb2116dc7a42bccba4e1f985e3b5b093bc
Sniffity <66455728+Sniffity@users.noreply.github.com>
2022-05-17 05:49:36 -0500

    PressureEqualizer -waterlog + correct asset

--- src/main/java/com/github/sniffity/panthalassa/server/block/BlockPressureEqualizer.java
+++ src/main/java/com/github/sniffity/panthalassa/server/block/BlockPressureEqualizer.java
@@ -1,19 +1,28 @@
 package com.github.sniffity.panthalassa.server.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.*;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.level.pathfinder.PathComputationType;
 import javax.annotation.Nullable;
 
 public class BlockPressureEqualizer extends Block implements EntityBlock {
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     public BlockPressureEqualizer() {
         super(Properties.of(
@@ -23,11 +32,25 @@ public class BlockPressureEqualizer extends Block implements EntityBlock {
                 .sound(SoundType.GLASS)
                 .noOcclusion()
                 .lightLevel((state) -> 15));
+        this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, Boolean.valueOf(false)));
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED);
+    }
+
+    public FluidState getFluidState(BlockState p_204507_1_) {
+        return p_204507_1_.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(p_204507_1_);
     }
 
     @Override
     public BlockEntity newBlockEntity(BlockPos p_153098_, BlockState p_153099_) {
         return new BlockPressureEqualizerBlockEntity(p_153098_, p_153099_);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState state) {
+        return RenderShape.ENTITYBLOCK_ANIMATED;
     }
 
     @Override
@@ -39,5 +62,12 @@ public class BlockPressureEqualizer extends Block implements EntityBlock {
 
     public boolean isPathfindable(BlockState p_52106_, BlockGetter p_52107_, BlockPos p_52108_, PathComputationType p_52109_) {
         return false;
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        BlockPos blockpos = context.getClickedPos();
+        FluidState fluidstate = context.getLevel().getFluidState(blockpos);
+        BlockState blockstate = this.defaultBlockState().setValue(WATERLOGGED, Boolean.valueOf(fluidstate.getType() == Fluids.WATER));
+        return blockstate;
     }
 }
