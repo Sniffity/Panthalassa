978e49a2da760ce2f4d866ec7917000b246de75f
Sniffity <66455728+Sniffity@users.noreply.github.com>
2022-10-19 00:29:19 -0500

    Attempt at fixing waterlogging

--- src/main/java/com/github/sniffity/panthalassa/server/registry/PanthalassaProcessors.java
+++ src/main/java/com/github/sniffity/panthalassa/server/registry/PanthalassaProcessors.java
@@ -2,7 +2,6 @@ package com.github.sniffity.panthalassa.server.registry;
 
 import com.github.sniffity.panthalassa.Panthalassa;
 import com.github.sniffity.panthalassa.server.world.processors.LaboratorySupportProcessor;
-import com.github.sniffity.panthalassa.server.world.processors.WaterloggingFixProcessor;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
@@ -10,10 +9,8 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProc
 public class PanthalassaProcessors {
 
     public static StructureProcessorType<LaboratorySupportProcessor> LABORATORY_SUPPORT_PROCESSOR = () -> LaboratorySupportProcessor.CODEC;
-    public static StructureProcessorType<WaterloggingFixProcessor> WATERLOGGING_FIX_PROCESSOR = () -> WaterloggingFixProcessor.CODEC;
 
     public static void registerProcessors() {
         Registry.register(Registry.STRUCTURE_PROCESSOR, new ResourceLocation(Panthalassa.MODID, "laboratory_support"), LABORATORY_SUPPORT_PROCESSOR);
-        Registry.register(Registry.STRUCTURE_PROCESSOR, new ResourceLocation(Panthalassa.MODID, "waterlogging_fix"), WATERLOGGING_FIX_PROCESSOR);
     }
 }
