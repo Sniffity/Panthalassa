bbfc3c69c450f49f77a4b49d1714c2e067ec56ee
Sniffity <Sniffity@yahoo.com>
2024-03-06 10:42:50 -0500

    Initial 1.20.4 commit - NeoForged

--- src/main/java/com/github/sniffity/panthalassa/server/entity/creature/ai/IHungry.java
+++
@@ -1,7 +0,0 @@
-package com.github.sniffity.panthalassa.server.entity.creature.ai;
-
-public interface IHungry {
-    void setHungerCooldown(float hungerCooldown);
-
-    float getHungerCooldown();
-}
