b5ca19462afc52f0e616313b496fc76cd5a855f3
Sniffity <Aldogmazzetti@gmail.com>
2024-04-17 03:02:06 -0500

    Extract conditional operations from tick method

--- src/main/java/com/github/sniffity/panthalassa/entity/creature/PanthalassaCreature.java
+++ src/main/java/com/github/sniffity/panthalassa/entity/creature/PanthalassaCreature.java
@@ -14,13 +14,43 @@ public abstract class PanthalassaCreature extends PathfinderMob {
         super.defineSynchedData();
     }
 
+
+
+    // =========================================
+    // Species Methods
+    // =========================================
+    protected abstract boolean speciesUnderwaterBreathing();
+
+    protected abstract boolean speciesDynamicYaw();
+
+    protected abstract boolean speciesAmphibious();
+
     @Override
     public void tick(){
         super.tick();
+        if (speciesUnderwaterBreathing()) {
+
+        }
+
+        if (speciesDynamicYaw()) {
+
+        }
+
+        if (speciesAmphibious()) {
+
+        }
     }
 
-    //SPECIES TYPE:
-    protected abstract boolean speciesUnderwaterBreathing();
+    private void yawOperations(){
+        //YAW OPERATIONS:
+        //The following lines of code handle the dynamic yaw animations for entities...
 
-    
+        //Grab the change in the entity's Yaw, deltaYRot...
+        //deltaYaw will tell us in which direction the entity is rotating...
+        //PENDING
+        //Store the previous yaw value, so we can use itn ext tick to calculate deltaYaw...
+        //PENDING
+
+    }
+
 }
