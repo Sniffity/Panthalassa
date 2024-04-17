04ad0f1d81090a89fb5fc6ce591f9afc4d972d66
Sniffity <Aldogmazzetti@gmail.com>
2024-04-17 03:53:58 -0500

    Begin implementation of lib methods

---
+++ src/main/java/com/github/sniffity/panthalassa/lib/entity/SniffityAquaticCreature.java
@@ -0,0 +1,15 @@
+package com.github.sniffity.panthalassa.lib.entity;
+
+import net.minecraft.world.entity.PathfinderMob;
+
+public class SniffityAquaticCreature {
+
+    //ToDo: Pending Extraction
+    public void handleDynamicAquaticYaw(PathfinderMob entity) {
+
+    }
+
+    public void handleDynamicAquaticPitch(PathfinderMob entity) {
+
+    }
+}
