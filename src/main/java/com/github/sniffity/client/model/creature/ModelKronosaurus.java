6c9410ac27b0bf66673b35d27e31c1a40bc1a92a
Sniffity <Sniffity@yahoo.com>
2025-06-15 02:03:14 -0500

    Create ModelKronosaurus.java

---
+++ src/main/java/com/github/sniffity/client/model/creature/ModelKronosaurus.java
@@ -0,0 +1,54 @@
+package com.github.sniffity.client.model.creature;
+
+import com.github.sniffity.Panthalassa;
+import com.github.sniffity.entity.creature.CreatureKronosaurus;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import software.bernie.geckolib.animation.AnimationState;
+import software.bernie.geckolib.cache.object.GeoBone;
+import software.bernie.geckolib.model.GeoModel;
+
+import javax.annotation.Nullable;
+import java.util.Optional;
+
+public class ModelKronosaurus extends GeoModel<CreatureKronosaurus> {
+
+    @Override
+    public ResourceLocation getModelResource(CreatureKronosaurus animatable) {
+        return ResourceLocation.fromNamespaceAndPath(Panthalassa.MODID,"geo/creature/kronosaurus/kronosaurus.geo.json");
+    }
+
+    @Override
+    public ResourceLocation getTextureResource(CreatureKronosaurus animatable) {
+        return ResourceLocation.fromNamespaceAndPath(Panthalassa.MODID,"textures/creature/kronosaurus/kronosaurus.png");
+    }
+
+    @Override
+    public ResourceLocation getAnimationResource(CreatureKronosaurus animatable) {
+        return ResourceLocation.fromNamespaceAndPath(Panthalassa.MODID,"animations/creature/kronosaurus/kronosaurus.json");
+    }
+
+
+    @Override
+    public void setCustomAnimations(CreatureKronosaurus entity, long instanceID, @Nullable AnimationState<CreatureKronosaurus> animationState) {
+        dynamicYaw1(entity, animationState);
+        super.setCustomAnimations(entity, instanceID,animationState);
+
+    }
+
+    public void dynamicYaw1(CreatureKronosaurus entity, AnimationState<CreatureKronosaurus> animationState){
+        float setYawValue = entity.prevSetYaw+(entity.setYaw-entity.prevSetYaw)*animationState.getPartialTick();
+        (this.getAnimationProcessor().getBone("lower_torso")).setRotY(setYawValue*5.0F);
+        (this.getAnimationProcessor().getBone("lower_torso_tail")).setRotY(setYawValue*5.0F);
+        (this.getAnimationProcessor().getBone("tail_section_1")).setRotY(setYawValue*5.0F);
+        (this.getAnimationProcessor().getBone("tail_section_2")).setRotY(setYawValue*5.0F);
+        (this.getAnimationProcessor().getBone("tail_section_3")).setRotY(setYawValue*5.0F);
+        (this.getAnimationProcessor().getBone("tail_section_4")).setRotY(setYawValue*5.0F);
+        (this.getAnimationProcessor().getBone("neck")).setRotY(-setYawValue*5.0F);
+    }
+
+    @Override
+    public Optional<GeoBone> getBone(String boneName) {
+        return super.getBone(boneName);
+    }
+}
